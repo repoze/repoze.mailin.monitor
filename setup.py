81097fa857e45aff6815a9805b59ad69c44c9316
Chris McDonough <chrism@plope.com>
2011-02-27 02:44:09 -0500

    make tests runnable, note github move, add contributors

--- setup.py
+++ setup.py
@@ -16,14 +16,17 @@ __version__ = '0.2-dev'
 
 import os
 
-from ez_setup import use_setuptools
-use_setuptools()
-
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+try:
+    README = open(os.path.join(here, 'README.txt')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except:
+    README = ''
+    CHANGES = ''
+    
 INSTALL_REQUIRES = [
     'repoze.bfg',
     'repoze.mailin',
